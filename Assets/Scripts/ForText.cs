e3840b3b5e0e28170a5e72d7753793648b3bb904
19이성진 <sagjin0000@naver.com>
2019-07-08 22:58:13 +0900

    텍스트 수치

---
+++ Assets/Scripts/ForText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ForText : MonoBehaviour
+{
+    [SerializeField]
+    private Text speedtext;
+    float speed;
+    int Intspeed;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.speedtext.text = "0";
+        speed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = speed + 5 * Time.deltaTime;
+        Intspeed = (int)speed;
+        this.speedtext.text = Intspeed.ToString();
+    }   
+    
+}
