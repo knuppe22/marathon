028b099913562389424d5e125ebbef23218ffd80
19이성진 <sagjin0000@naver.com>
2019-07-10 16:24:38 +0900

    텍스트 수치 저장 및 로드

--- Assets/Scripts/ForText.cs
+++ Assets/Scripts/ForText.cs
@@ -2,19 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class ForText : MonoBehaviour
 {
     [SerializeField]
     private Text speedtext;
     float speed;
+    float textspeed = 0;
     int Intspeed;
- 
+    int Inttextspeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        this.speedtext.text = "0";
-        speed = 0;
+        string _Filestr = "Assets/Resources/Data.txt";
+        System.IO.FileInfo fi = new
+            System.IO.FileInfo(_Filestr);
+
+        if (fi.Exists)
+        {
+            Parse();
+            speed = textspeed;
+            Intspeed = (int)textspeed;
+            this.speedtext.text = Inttextspeed.ToString();
+        }
+
+        else
+        {
+            speedtext.text = "0";
+            speed = 0;
+        }
+    }
+
+    string m_strPath = "Assets/Resources/";
+
+    public void WriteData(string strData)
+    {
+        FileStream f = new FileStream(m_strPath + "Data.txt", FileMode.Create, FileAccess.Write);
+
+        StreamWriter writer = new StreamWriter(f, System.Text.Encoding.Unicode);
+
+        writer.WriteLine(strData);
+
+        writer.Close();
+    }
+
+    public void Parse()
+    {
+        TextAsset data = Resources.Load("Data", typeof(TextAsset)) as TextAsset;
+        StringReader sr = new StringReader(data.text);
+
+        textspeed = float.Parse(sr.ReadLine());
+
+        sr.Close();
     }
 
     // Update is called once per frame
@@ -23,6 +64,10 @@ public class ForText : MonoBehaviour
         speed = speed + 5 * Time.deltaTime;
         Intspeed = (int)speed;
         this.speedtext.text = Intspeed.ToString();
-    }   
-    
+    }
+
+    private void OnApplicationQuit()
+    {
+        WriteData(speed.ToString());
+    }
 }
