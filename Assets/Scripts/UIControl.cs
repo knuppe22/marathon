06cf44ec23480f19cc200a01fe9a158cfb1ddcbf
siftheads <siftheads@korea.ac.kr>
2019-07-20 12:45:50 +0900

    하나의 UI 창만 띄우기 & 친구 추가, 손 잡기 성공창은 아무데나 눌러도 사라짐

--- Assets/Scripts/UIControl.cs
+++ Assets/Scripts/UIControl.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class UIControl : MonoBehaviour
 {
+    public GameObject[] SuccessPanel = new GameObject[2];
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +14,21 @@ public class UIControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            for (int i=0; i<2; i++)
+            {
+                if (SuccessPanel[i].gameObject.activeSelf)
+                {
+                    SuccessPanel[i].SetActive(false);
+                }
+            }
+        }
     }
-    public void OnOff()
+    public void OnOff(GameObject OtherPanel)
     {
         gameObject.SetActive(!gameObject.activeSelf);
+        if (OtherPanel.gameObject.activeSelf)
+            OtherPanel.gameObject.SetActive(false);
     }
 }
