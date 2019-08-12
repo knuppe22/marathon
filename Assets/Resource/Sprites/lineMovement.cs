bac96453fbd07fe4d562789665f141eddbfbbb27
marine0964 <marine0664@gmail.com>
2019-08-12 15:58:24 +0900

    피드백용 커밋
    임시 프리팹, 움직이는 도로작업중이던 파일들

---
+++ Assets/Resource/Sprites/lineMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineMovement : MonoBehaviour {
+    public int speed;
+    public GameObject line;
+    public GameObject bg;
+
+    void Start() {
+        Instantiate(line, bg.transform.position);
+    }
+    
+    void Update() {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        transform.Translate(Vector2.down * 965 / 1000 * speed * Time.deltaTime);
+
+        if (transform.position.x < -5)
+            Destroy(line);
+    }
+}
