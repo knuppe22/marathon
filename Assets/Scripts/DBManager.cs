a9f18337217b63d5e84fd6c145107cc9fb88b082
16도재형 <knuppe22@gmail.com>
2019-08-20 22:04:48 +0900

    지리정보 DB에서 관리하게 함, RunManager에서 관리하게 해야함

--- Assets/Scripts/DBManager.cs
+++ Assets/Scripts/DBManager.cs
@@ -17,6 +17,14 @@ public class DBManager : SingletonBehaviour<DBManager>
     {
         get { return UserReference.Child(AuthManager.Instance.CurrentUserId); }
     }
+    public DatabaseReference LocationReference
+    {
+        get { return RootReference.Child("locations"); }
+    }
+    public DatabaseReference CurrentLocationReference
+    {
+        get { return LocationReference.Child(AuthManager.Instance.CurrentUserId); }
+    }
 
     public void SetDatabase()
     {
@@ -24,8 +32,7 @@ public class DBManager : SingletonBehaviour<DBManager>
 
         RootReference = FirebaseDatabase.DefaultInstance.RootReference;
     }
-
-    // TODO: 여러가지 DB 정보 get, set 하는 함수 만들기
+    
     public async Task<User> GetUser()
     {
         return await GetUser(AuthManager.Instance.CurrentUserId);
@@ -65,7 +72,40 @@ public class DBManager : SingletonBehaviour<DBManager>
     public void SetUserValue(string key, object value)
     {
         // key에 들어갈 수 있는 것들: "name", "score", "gold", "online", "lastOnline", "friends", "items", "equippedItems"
+        // 위치정보 불가!!!
         Debug.Log("ValueSet " + key);
         CurrentUserReference.Child(key).SetValueAsync(value);
+    }
+
+    public void SetLocation(float latitude, float longitude)
+    {
+        CurrentLocationReference.Child("latitude").SetValueAsync(latitude);
+        CurrentLocationReference.Child("longitude").SetValueAsync(longitude);
+    }
+
+    public async Task<Dictionary<string, GeoCoord>> GetLocations()
+    {
+        Task<DataSnapshot> task = LocationReference.GetValueAsync();
+        await task;
+
+        if (task.IsFaulted)
+        {
+            Debug.LogErrorFormat("GetLocation() failed");
+        }
+        else if (task.IsCompleted)
+        {
+            if (task.Result.Exists)
+            {
+                Debug.LogFormat("GetLocation() succeeded");
+                return JsonUtility.FromJson<Dictionary<string, GeoCoord>>(task.Result.GetRawJsonValue());
+            }
+            else
+            {
+                Debug.LogFormat("GetLocation() succeeded but no data");
+                return new Dictionary<string, GeoCoord>();
+            }
+        }
+
+        return null;
     }
 }
---
+++ Assets/Scripts/GeoCoord.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GeoCoord
+{
+    public float latitude;
+    public float longitude;
+
+    public static float GetDistance(GeoCoord p1, GeoCoord p2)
+    {
+        float radius = 6371f;
+        float dLat = Mathf.Deg2Rad * (p2.latitude - p1.latitude);
+        float dLon = Mathf.Deg2Rad * (p2.longitude - p1.longitude);
+
+        float a = Mathf.Sin(dLat / 2) * Mathf.Sin(dLat / 2)
+                  + Mathf.Cos(Mathf.Deg2Rad * p1.latitude) * Mathf.Cos(Mathf.Deg2Rad * p2.latitude)
+                  * Mathf.Sin(dLon / 2) * Mathf.Sin(dLon / 2);
+        float c = 2 * Mathf.Atan2(Mathf.Sqrt(a), Mathf.Sqrt(1 - a));
+        return radius * c;
+    }
+}
